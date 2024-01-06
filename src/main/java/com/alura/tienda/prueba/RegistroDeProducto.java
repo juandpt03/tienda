afc8bbdb381b09cc76f62a5798a919e265c5375e
juandpt03 <juandpt03@gmail.com>
2024-01-06 11:45:37 -0500

    implemented queries

--- src/main/java/com/alura/tienda/prueba/RegistroDeProducto.java
+++ src/main/java/com/alura/tienda/prueba/RegistroDeProducto.java
@@ -1,20 +1,51 @@
 package com.alura.tienda.prueba;
 
+import com.alura.tienda.dao.CategoriaDao;
+import com.alura.tienda.dao.ProductoDao;
+import com.alura.tienda.modelo.Categoria;
 import com.alura.tienda.modelo.Producto;
+import com.alura.tienda.utils.JPAUtils;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 import java.math.BigDecimal;
+import java.util.List;
 
 public class RegistroDeProducto {
     public static void main(String[] args) {
-        Producto celular  = new Producto();
-        celular.setNombre("Samsung");
-        celular.setDescripcion("Telefono usado");
-        celular.setPrecio(new BigDecimal("1000"));
-        EntityManagerFactory factory  = Persistence.createEntityManagerFactory("tienda");
-        EntityManager entityManager = factory.createEntityManager();
-        entityManager.persist(celular);
+        registrarProducto();
+        EntityManager entityManager =  JPAUtils.getEntityManager();
+        ProductoDao productoDao = new ProductoDao(entityManager);
+
+
+        Producto producto = productoDao.consultaPorId(1L);
+        System.out.println(producto.getNombre());
+
+        List<Producto> productos = productoDao.consultaPorNombre("Xiaomi Redmi");
+        List<Producto> productos1 = productoDao.consultaPorNombreDeCategoria("CELULARES");
+        productos.forEach(producto1 -> System.out.println(producto1.getDescripcion()));
+        productos1.forEach(producto1 -> System.out.println(producto1.getNombre()));
+        BigDecimal precio = productoDao.consultarPrecioPorNombreDeProducto("Xiaomi Redmi");
+        System.out.println(precio);
+
+
+
+    }
+    private static void registrarProducto(){
+        EntityManager entityManager =  JPAUtils.getEntityManager();
+        ProductoDao productoDao = new ProductoDao(entityManager);
+        CategoriaDao categoriaDao = new CategoriaDao(entityManager);
+
+        Categoria celulares = new Categoria("CELULARES");
+        Producto celular = new Producto("Xiaomi Redmi", "Legal este producto",new BigDecimal("1000"),celulares);
+
+        entityManager.getTransaction().begin();
+        categoriaDao.guardar(celulares);
+        productoDao.guardar(celular);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+
+
     }
 }
